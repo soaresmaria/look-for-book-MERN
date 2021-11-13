19a1e557815ff0ab6345e148cf2ece64e5ce08b7
Mona Lisa <soaresmaria181@yahoo.com>
2021-11-13 02:17:33 -0500

    updated server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const path = require('path');
+//import apollo server
+const { ApolloServer } = require('apollo-server-express');
+// import typeDefs and resolvers
+const { typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+//apply apollo server with express app
+server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +32,14 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+//get all
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+});
 });
