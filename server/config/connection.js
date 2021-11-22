0f7c69b778fe3bfa949c6ef92abec09b57daa7a7
Mona Lisa <soaresmaria181@yahoo.com>
2021-11-21 20:23:14 -0500

    testing error for deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookhunt04', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/look-for-book-MERN', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
